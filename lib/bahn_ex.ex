7689e707a61e21c175251645120ca914f50876b6
pfitz <info@pfiss.com>
2017-08-10 09:19:28 +0200

    Initial commit

---
+++ lib/bahn_ex.ex
@@ -0,0 +1,18 @@
+defmodule BahnEx do
+  @moduledoc """
+  Documentation for BahnEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BahnEx.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,32 @@
+defmodule BahnEx.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :bahn_ex,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:httpoison, "~> 0.13.0"},
+      {:poison, "~> 3.1"},
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+end
