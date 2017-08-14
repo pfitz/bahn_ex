4948e9107a37d4ee3027226578b958059a88524c
pfitz <info@pfiss.com>
2017-08-14 09:10:38 +0200

    Bump version to 1.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BahnEx.Mixfile do
   def project do
     [
       app: :bahn_ex,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
